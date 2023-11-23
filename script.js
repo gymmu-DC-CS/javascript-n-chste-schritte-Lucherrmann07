c4d68c0fa8138cbc50ec86165023b2020d47229b
Luc Herrmann <lucherrmann1@gmail.com>
2023-11-23 12:15:14 +0100

    commit

--- script.js
+++ script.js
@@ -17,3 +17,16 @@ export function aufgabe02(args) {
   const result = input.toUpperCase()
   return result
 }
+
+export function aufgabe03(args) {
+  const input = args
+  const result = 0
+
+for (let i = 0; i < input.length; i++)  {
+  const currentElement = input[i]
+  if (currentElement === "e" || currentElement === "E") {
+    result++
+  }
+  }
+  return result
+  }
